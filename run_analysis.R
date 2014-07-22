86bbfa52789c242acfad3fa085ce9783676b06bc
Alexander Dovzhikov <alexander.dovzhikov@gmail.com>
2014-07-23 08:55:21 +1000

    Asquith -> SYD

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,7 @@
 data.dir <- "UCI HAR Dataset"
 Xtrain.fname <- paste0(data.dir, "/train/X_train.txt")
 ytrain.fname <- paste0(data.dir, "/train/y_train.txt")
+subjecttrain.fname <- paste0(data.dir, "/train/subject_train.txt")
 Xtest.fname <- paste0(data.dir, "/test/X_test.txt")
 ytest.fname <- paste0(data.dir, "/test/y_test.txt")
 subjecttest.fname <- paste0(data.dir, "/test/subject_test.txt")
@@ -9,19 +10,24 @@ features.fname <- paste0(data.dir, "/features.txt")
 
 Xtrain <- read.table(Xtrain.fname)
 ytrain <- read.table(ytrain.fname)
+subjecttrain <- read.table(subjecttrain.fname)
 Xtest <- read.table(Xtest.fname)
 ytest <- read.table(ytest.fname)
 subjecttest <- read.table(subjecttest.fname)
 activitylabels <- read.table(activitylabels.fname)
 features <- read.table(features.fname)
 
-train <- cbind(ytrain, Xtrain)
-test <- cbind(ytest, Xtest)
+train <- cbind(ytrain, subjecttrain, Xtrain)
+test <- cbind(ytest, subjecttest, Xtest)
 
 data <- rbind(train, test)
-var.names <- c("ActivityId", as.character(features[,2]))
+var.names <- c("Activity", "Subject", as.character(features[,2]))
 colnames(data) <- var.names
 meanStdCols <- grep("(mean|std)\\(\\)", var.names)
-data <- data[, c(1, meanStdCols)]
+data <- data[, c(1:2, meanStdCols)]
 
-colnames(activitylabels) <- c("ActivityId", "Activity")
+agg <- aggregate(. ~ Activity + Subject, datd, mean)
+
+data[,1] <- sapply(data[,1], function(i) { 
+    activitylabels[match(i, activitylabels[,1]), 2]
+})
